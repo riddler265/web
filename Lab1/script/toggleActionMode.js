1dd8720b498723bc095b6f868386098a68791023
riddler265 <egoor120@gmail.com>
2026-09-23 22:45:40 +0300

    Метод, срабатывающий при нажатии toggleButton. Подумать над хранением данных.

--- Lab1/script/toggleActionMode.js
+++ Lab1/script/toggleActionMode.js
@@ -1,12 +1,16 @@
-export function toogleActionMode() {
-    let isActionMode = localStorage.getItem('isActionMode') ?? false;
+export function toggleActionMode() {
+    const isActionMode = JSON.parse(localStorage.getItem('isActionMode') ?? 'false');
 
     if (isActionMode) {
         localStorage.setItem('fillColor', '#3124c7');
-        localStorage.setItem('borderColor', '#38aef2');
+        localStorage.setItem('strokeColor', '#38aef2');
+        document.documentElement.style.setProperty('--fill-color', '#3124c7');
+        document.documentElement.style.setProperty('--stroke-color', '#38aef2');
     } else {
         localStorage.setItem('fillColor', '#216817');
-        localStorage.setItem('borderColor', '#68fd51');
+        localStorage.setItem('strokeColor', '#68fd51');
+        document.documentElement.style.setProperty('--fill-color', '#216817');
+        document.documentElement.style.setProperty('--stroke-color', '#68fd51');
     }
-    localStorage.setItem('isActionMode', `${!isActionMode}`);
+    localStorage.setItem('isActionMode', JSON.stringify(!isActionMode));
 }
