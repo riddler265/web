f8f6aa3796d673b2128385631d07050b88a11844
riddler265 <egoor120@gmail.com>
2026-09-23 22:46:49 +0300

    Разделение ответственности. Исправление логики. Пока продолжаю работать.

--- Lab1/script/script.js
+++ Lab1/script/script.js
@@ -1,78 +1,53 @@
 import Decimal from 'https://cdn.jsdelivr.net/npm/decimal.js@10.4.3/+esm';
 
 import { toggleActionMode } from './toggleActionMode.js';
+import { drawDots, drawPlane } from './draw.js';
+import { validateY, validateR, validateX } from './validators.js';
+import { HitRecord } from './classes/hitRecord.js';
+import { Dot } from './classes/dot.js';
 
-const availableR = [1, 1.5, 2, 2.5, 3];
-const availableY = [-2, -1.5, -1, -0.5, 0, 0.5, 1, 1.5, 2];
+
 const hitRecordSet = new Set();
-
-const canvas = document.getElementById('coordinatePlane');
-const ctx = canvas.getContext('2d');
-
-const centerX = canvas.width / 2;
-const centerY = canvas.height / 2;
-
-let lastParametrR;
 
 const hitRecordTable = document.getElementById('hitRecordTable');
 
 const shootForm = document.getElementById('shootForm');
 const clearButton = document.querySelector('.clearButton');
-const toggleButton = document.querySelector('toggleButton');
+const toggleButton = document.querySelector('.toggleButton');
 
 const inputX = document.querySelector('.parametrXInput');
 
-class Dot {
-
-    constructor(x, y) {
-        this.x = x;
-        this.y = y;
-    }
-
-    draw(division) {
-        ctx.beginPath();
-        ctx.moveTo(centerX + division * this.x, centerY - division * this.y);
-        ctx.arc(centerX + division * this.x, centerY - division * this.y, 4, 0, 2 * Math.PI);
-        ctx.fillStyle = '#ad3b3b';
-        ctx.strokeStyle = 'rgb(255, 255, 255)';
-        ctx.lineWidth = 2;
-        ctx.closePath;
-        ctx.stroke();
-        ctx.fill();
-        ctx.lineWidth = 1;
-    }
-}
-
-class HitRecord {
-
-    constructor(dot, radius, timestamp) {
-        this.dot = dot;
-        this.radius = radius;
-        this.hit = isHit(dot, radius);
-        this.timestamp = timestamp;
-    }
-}
-
 loadHitRecords();
-drawPlane(getLastParametrR());
+init();
 
 shootForm.addEventListener('submit', function(event) {
     event.preventDefault();
 
 
     try {
+
         const formData = new FormData(this);
 
         const parametrR = validateR(formData.get('ParametrR'));
         const parametrX = validateX(formData.get('ParametrX'));
-        const parametrY = validateY(formData.get('ParametrY')); 
+        const parametrY = validateY(formData.get('ParametrY'));
 
-        const hitRecord = new HitRecord(new Dot(parametrX, parametrY), parametrR, Date.now())
+        console.log(`Получены параметры.\nX: ${parametrX}\nY: ${parametrY}\nRadius: ${parametrR}`);
 
+        const dot = new Dot(parametrX, parametrY);
+        const hitRecord = new HitRecord(dot, parametrR, Date.now());
         hitRecordSet.add(hitRecord);
+
+        console.log(`Произошел выстрел.\nПопадание: ${hitRecord.hit}\nВремя: ${hitRecord.timestamp}`)
+
         createHitRecord(hitRecord);
         saveData(parametrR);
+
         drawPlane(parametrR);
+        drawDots(hitRecordSet);
+
+
+        
         inputX.classList.remove('error');
         inputX.setAttribute('placeholder', 'Введите X');
     } catch (error) {
@@ -97,130 +72,21 @@ clearButton.addEventListener('click', function() {
                     </thead>
                     <tbody id="resultsTableBody">
                     </tbody>`;
-    drawPlane(lastParametrR);
+    drawPlane(JSON.parse(localStorage.getItem('lastParametrR')));
 });
 
-toggleButton.addEventListener('click', toggleActionMode());
+toggleButton.addEventListener('click', function() {
+    toggleActionMode();
+    drawPlane(JSON.parse(localStorage.getItem('lastParametrR')));
+    drawDots(hitRecordSet);
+});
 
-function validateX(x) {
-    const lowBound = new Decimal(-5);
-    const upBound = new Decimal(3);
+function init() {
+    toggleActionMode();
+    toggleActionMode();
+    drawPlane(JSON.parse(localStorage.getItem('lastParametrR') ?? '1'));
 
-    try {
-        const validX = new Decimal(x);
-
-        if (validX.gt(lowBound) && validX.lt(upBound)) return validX;
-        else throw new Error('Неподходящее значение параметра X!');
-    } catch (error) {
-        throw new Error('Неподходящее значение параметра X!');
-    }
-}
-
-function validateY(y) {
-    for (let i = 0; i < availableY.length; i++) {
-        if (y == availableY[i]) return Number(y);
-    } throw new Error('Неподходящее значение параметра Y!');
-}
-
-function validateR(multiplier) {
-    for (let i = 0; i < availableR.length; i++) {
-        if (multiplier == availableR[i]) return Number(multiplier);
-    } throw new Error('Неподходящее значениие параметра R!');
-}
-
-function isHit(dot, parametrR) {
-    const r = new Decimal(parametrR);
-    let x = dot.x;
-    let y = dot.y;
-
-    console.log(typeof x);
-
-    return (x >= 0 && y >= 0 && x + y <= r) ||
-    (x <= 0 && x >= -r && y >= 0 && y <= r) ||
-    (x <= 0 && y <= 0 && x * x + y * y <= r * r);
-}
-
-function drawPlane(parametrR) {
-    lastParametrR = parametrR;
-
-    const margin = 10;
-    const division = 75;
-    const radius = division * parametrR;
-
-    ctx.fillStyle = "rgb(0, 0, 0)";
-    ctx.fillRect(0, 0, canvas.width, canvas.height);
-
-    ctx.beginPath();
-    ctx.moveTo(centerX + radius, centerY);
-    ctx.lineTo(centerX, centerY - radius);
-    ctx.lineTo(centerX - radius, centerY - radius);
-    ctx.lineTo(centerX - radius, centerY);
-    ctx.moveTo(centerX, centerY);
-    ctx.arc(centerX, centerY, radius, Math.PI, 0.5 * Math.PI, true);
-    ctx.closePath();
-    ctx.fillStyle = 'rgb(33, 104, 23)';
-    ctx.strokeStyle = 'rgb(104, 253, 81)';
-    ctx.lineWidth = 2;
-    ctx.stroke();
-    ctx.fill();
-
-    ctx.strokeStyle = "rgb(104, 253, 81)";
-    ctx.font = "14px Arial";
-    ctx.fillStyle = 'rgb(104, 253, 81)';
-    ctx.lineWidth = 1;
-
-    ctx.beginPath();
-    ctx.moveTo(centerX, margin);
-    ctx.lineTo(centerX, canvas.height - margin);
-    ctx.moveTo(centerX - 5, margin + 5);
-    ctx.lineTo(centerX, margin);
-    ctx.lineTo(centerX + 5, margin + 5);
-    ctx.fillText('Y', centerX + 10, margin + 7);
-
-    ctx.moveTo(margin, centerY);
-    ctx.lineTo(canvas.width - margin, centerY);
-    ctx.lineTo(canvas.width - margin - 5, centerY + 5);
-    ctx.moveTo(canvas.width - margin, centerY);
-    ctx.lineTo(canvas.width - 5 - margin, centerY - 5);
-    ctx.fillText('X', canvas.width - margin - 5, centerY + 20)
-    ctx.stroke();   
-
-    ctx.beginPath();
-    ctx.font = "10px Arial";
-
-
-    for (let i = -10; i < 11; i++) {
-
-        if (i != 0) {
-            ctx.moveTo(centerX + i * division / 2, centerY + 3);
-            ctx.lineTo(centerX + i * division / 2, centerY - 3);
-            
-        }
-        
-        if (i != 0) {
-            ctx.fillText(i * 0.5, centerX + i * division / 2, centerY - 5);
-        }
-    }
-
-    for (let i = -6; i < 7; i++) {
-
-        if (i != 0) {
-            ctx.moveTo(centerX + 3, centerY + i * division / 2);
-            ctx.lineTo(centerX - 3, centerY + i * division / 2);
-        }
-        
-        if (i != 0) {
-            ctx.fillText(-i * 0.5, centerX + 5, centerY + i*division / 2 + 3);
-        }
-    }
-
-    ctx.stroke();
-    ctx.beginPath();
-
-    for (const hitRecord of hitRecordSet) {
-        hitRecord.dot.draw(division);
-    }
-
+    drawDots(hitRecordSet);
 }
 
 function loadHitRecords() {
@@ -268,10 +134,6 @@ function createHitRecord(hitRecord) {
     tr.appendChild(tdTimestamp);
 
     hitRecordTable.prepend(tr);
-}
-
-function getLastParametrR() {
-    return lastParametrR = JSON.parse(localStorage.getItem('lastParametrR'));
 }
 
 function saveData(parametrR) {
